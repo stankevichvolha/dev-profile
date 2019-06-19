fe2e2eeb2cf5c85842868c88e19391b041509881
Volha Stankevich <stankevichvolhaig@gmail.com>
2019-06-19 09:12:24 -0700

    initial project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Volha  Stankevich on 6/19/19.
+//  Copyright © 2019 Volha  Stankevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
